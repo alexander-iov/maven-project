8db015e365aacbd51aaed33b87709b00599d0f6b
alexander-iov <sasha.iovsa@gmail.com>
2021-01-26 21:16:02 +0200

    Complete task

--- src/main/java/src/Main.java
+++ src/main/java/src/Main.java
@@ -1,7 +1,21 @@
 package src;
 
+import org.apache.commons.collections4.CollectionUtils;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+        String[] strings = {"a","b","c"};
+        CollectionUtils.reverseArray(strings);
+        System.out.println(Arrays.toString(strings));
 
+        List<String> emptyList = new ArrayList<>();
+        System.out.println(CollectionUtils.isEmpty(emptyList));
+        System.out.println(CollectionUtils.isEmpty(null));
+
+        emptyList.addAll(Arrays.asList(strings));
+        System.out.println(CollectionUtils.isEmpty(emptyList));
     }
 }
